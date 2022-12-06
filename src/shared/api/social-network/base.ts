60ae454ae71aa7f64e6eab3449b5d8db5480fc98
pavel-developer2001 <gaidukpavel18@gmail.com>
2022-12-06 08:10:57 +0300

    add interceptor

--- src/shared/api/social-network/base.ts
+++ src/shared/api/social-network/base.ts
@@ -1,19 +1,43 @@
 import axios from "axios";
 import { API_URL } from "../../config";
+import AuthApi from "./apis/auth-api";
 
 const token =
   typeof window !== "undefined" &&
   window.localStorage.getItem("social-network");
 
-const ReaderApi = axios.create({
+const instance = axios.create({
   baseURL: API_URL,
   withCredentials: true,
 });
 
-ReaderApi.interceptors.request.use((config) => {
+instance.interceptors.request.use((config) => {
   //@ts-ignore
   config.headers.Authorization = `Bearer ${token}`;
   return config;
 });
+instance.interceptors.response.use(
+  (config) => {
+    return config;
+  },
+  async (error) => {
+    const originalRequest = error.config;
+    if (
+      error.response.status == 401 &&
+      error.config &&
+      !error.config._isRetry
+    ) {
+      originalRequest._isRetry = true;
+      try {
+        const response = await AuthApi.refresh();
+        localStorage.setItem("social-network", response.data.accessToken);
+        return instance.request(originalRequest);
+      } catch (e) {
+        console.log("НЕ АВТОРИЗОВАН");
+      }
+    }
+    throw error;
+  }
+);
 
-export default ReaderApi;
+export default instance;
